1fb84f429633613bb976d688e38476279d036d91
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2023-10-05 00:30:04 +0100

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,3 +4,18 @@
 #include <stddef.h>
 
 /**
+ * _strdup - Duplicates a string
+	* @str: The pointer to the string to duplicate
+ *
+	*Return: Duplictaed string
+ */
+char *_strdup(char *str)
+{
+	char *b;
+	unsigned int i, j;
+
+	i = 0, j = 1;
+
+	if (str == NULL)
+	return (NULL);
+    
